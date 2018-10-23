a5062e768f9e7f0096c961cda423345edeb60f3e
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-23 19:56:31 +0900

    [fix]足りないimportを追加

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,7 @@ import javafx.application.Application
 import javafx.event.{ActionEvent, EventHandler}
 import javafx.scene.Scene
 import javafx.scene.control.Button
+import javafx.scene.layout.StackPane
 import javafx.stage.Stage
 
 object Main extends App {
